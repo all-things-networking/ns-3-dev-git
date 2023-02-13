f74d79e9a9f5867a57e38f66550275ab64c6dcfe
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-02-13 15:28:10 -0500

    Update TCP-scheduler.h

--- contrib/modular-transport/model/TCP-scheduler.h
+++ contrib/modular-transport/model/TCP-scheduler.h
@@ -1,8 +1,10 @@
 #ifndef MT_TCP_SCHEDULER_H
 #define MT_TCP_SCHEDULER_H
+#include "mt-scheduler.h"
 namespace ns3{
 class MTScheduler;
 class MTEvent;
+class MTHeader
 class TCPscheduler: public MTScheduler{
 struct MTEventQueue;
     void OpsBeforeSend(); // Compute Hash
@@ -11,7 +13,7 @@ struct MTEventQueue;
     MTEvent GenerateEventOnInitate(MTHeader);
     MTEvent GetNextEvent();
     MTEvent CreateSendEvent();
-    }
+    };
 }
-}
+
 #endif
