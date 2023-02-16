286d60fbf151aedbbeffea64e5e752ee46fd534b
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-02-16 14:24:01 -0500

    Update mt-event.cc

--- contrib/modular-transport/model/mt-event.cc
+++ contrib/modular-transport/model/mt-event.cc
@@ -5,7 +5,7 @@ MTEvent::MTEvent(long time, int flow_id){
     this->flow_id=flow_id;
 }
 SendEvent::SendEvent(long time, int flow_id){
-    super(time,flow_id);
+    this->time=time;
+    this->flow_id=flow_id;
 }
-
 }
