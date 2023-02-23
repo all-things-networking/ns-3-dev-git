561735d648987fa77784b23c0b909bda101d0135
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-02-23 15:25:10 -0500

    TCP event processor

---
+++ contrib/modular-transport/model/TCP-eventprocessor.cc
@@ -0,0 +1,46 @@
+#include "mt-eventprocessor.h"
+#include "mt-state.h"
+#include "mt-event.h"
+#include "TCP-eventprocessor.h"
+#include "TCP-context.h"
+#include <vector>
+#include <utility>   // std::pair
+#include <algorithm> // std::min, std::max
+#include "ns3/ipv4-l3-protocol.h"
+#include "ns3/node.h"
+namespace ns3
+{
+class Packet;
+class ModularTransport;
+//Send if possible, mine
+SendIfPossible::SendIfPossible():
+MTEventProcessor()
+{}
+bool
+SendIfPossible::IsValidEvent(MTEvent e)
+{
+    return true;
+}
+
+EventProcessorOutput* SendIfPossible::Process(MTEvent e, MTContext* c){
+    //I call mt->SendPack here
+    TcpContext* newContext = dynamic_cast<TcpContext*>(c);
+    //A
+    std::vector<MTEvent> newEvents;
+
+    //New Packets
+    //Add window
+    std::vector<Packet> packetTobeSend;
+    Packet P = Packet(newContext->data, 4);
+    packetTobeSend.emplace_back(P);
+
+    //Output
+    EventProcessorOutput *Output = new EventProcessorOutput;
+    Output->newEvents=newEvents;
+    Output->updatedContext=newContext;
+    Output->packetToSend=packetTobeSend;
+    return Output;
+    //store packets to send as vector in class
+    //call get packet to retrieve it later, and clear vector in class, use temp vector
+}
+} // namespace ns3
---
+++ contrib/modular-transport/model/TCP-eventprocessor.h
@@ -0,0 +1,87 @@
+#ifndef TCP_EVENTPROCESSOR_H
+#define TCP_EVENTPROCESSOR_H
+#include "mt-eventprocessor.h"
+#include <vector>
+#include <utility> // std::pair
+
+//mostly copied from eric's branch, though send if possible is mine
+namespace ns3
+{
+
+class MTEvent;
+class MTContext;
+class Packet;
+/**
+ * \brief The base class for event processor. This is virtual class.
+ */
+struct EventProcessorOutput;
+class MTEventProcessor;
+
+
+/**
+ * \brief The processor handling the acknowledgement events.
+ */
+class TcpAckHandler: public MTEventProcessor
+{
+public:
+    TcpAckHandler();
+
+    /**
+     * \brief Process the event and return the processed results.
+     * \param e The input event to be processed.
+     * \param c The context of the Mt connection.
+     * \return The modified context and new generated event if exits.
+     *
+     * Discard the Ack Event when acknowledge number is invalid.
+     * Active fact retrainsmit when duplicte Ack received.
+     * Timeout lost should be handled by other event processors.
+     * Perform Mt congestion control.
+     */
+    EventProcessorOutput* Process(MTEvent e, MTContext* c);
+
+
+    /**
+     * \brief Check if the input event is valid event type for the processor.
+     * \param e The input event to be processed.
+     * \return True if input event type matches the processor type, false otherwise.
+     */
+    bool IsValidEvent(MTEvent e);
+};
+
+/**
+ * \brief The processor managing congestion control algorithms.
+*/
+class TcpCongControl: public MTEventProcessor
+{
+public:
+    TcpCongControl();
+
+    /**
+     * \brief Process the event and return the processed results.
+     * \param e The input event to be processed.
+     * \param c The context of the Mt connection.
+     * \return The modified context and new generated event if exits.
+     *
+     * Perform Mt congestion control based on RFC5681.
+     */
+    EventProcessorOutput* Process(MTEvent e, MTContext* c);
+
+    /**
+     * \brief Check if the input event is valid event type for the processor.
+     * \param e The input event to be processed.
+     * \return True if input event type matches the processor type, false otherwise.
+     */
+    bool IsValidEvent(MTEvent e);
+};
+
+class SendIfPossible: public MTEventProcessor
+{
+public:
+    SendIfPossible();
+    EventProcessorOutput* Process(MTEvent e, MTContext* c);
+    bool IsValidEvent(MTEvent e);
+};
+
+} // namespace ns3
+
+#endif
