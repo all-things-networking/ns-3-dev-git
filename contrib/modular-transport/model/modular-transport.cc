67d6cd2ecd47917866e744aff99e719569edcf46
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-02-23 14:34:17 -0500

    Update modular-transport.cc

--- contrib/modular-transport/model/modular-transport.cc
+++ contrib/modular-transport/model/modular-transport.cc
@@ -180,7 +180,7 @@ ModularTransport::Receive(Ptr<Packet> packet,
     packet->RemoveHeader(recievedHeader);
     uint8_t *buffer = new uint8_t[packet->GetSize()];
     int size = packet->CopyData(buffer, packet->GetSize());
-    NS_LOG_UNCOND("Received:"<<buffer[0]);
+    NS_LOG_UNCOND("Received:"<<size<<" "<buffer[0]);
     //chosenScheduler.OpsAfterRecieved(recievedHeader);
     //chosenScheduler.GenerateEventOnReceive(recievedHeader);
     //recievedHeader.OpsAfterRecieved(); //THis one returns a event
