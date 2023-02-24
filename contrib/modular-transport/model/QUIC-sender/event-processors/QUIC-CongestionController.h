31f1af9f5b4fa7fdf75be7d6c9a1a3aadf5ba614
Kevin Bian <yqbian@green03.syn.uwaterloo.ca>
2023-02-24 00:14:22 +0000

    updating some more header files

--- contrib/modular-transport/model/QUIC-sender/event-processors/QUIC-CongestionController.h
+++ contrib/modular-transport/model/QUIC-sender/event-processors/QUIC-CongestionController.h
@@ -15,15 +15,26 @@ class MTEvent;
 class MTEventQueue;
 class MTHeader;
 
+enum TcpCongestionControlAlgorithm {
+  TCPNewReno,
+  Cubic
+};
+
 /**
  * \brief The processor handling the congestion control events.
  */
 class QUICCongestionController: public QUICEventProcessor
 {
+private:
+    int windowSize;
+    TcpCongestionControlAlgorithm congestionControlAlgorithm = TCPNewReno; // Default value is TCPNewReno
 public:
     QUICCongestionController();
     EventProcessorOutput* Process(MTEvent e, MTContext* c);
     bool IsValidEvent(MTEvent e);
+    void SetWindowSize(int value);
+    int GetWindowSize();
+    void SetCongestionControlAlgorithm(TcpCongestionControlAlgorithm algorithm);
 };
 } // namespace ns3
 #endif
--- contrib/modular-transport/model/QUIC-sender/event-processors/QUIC-SendIfPossible.h
+++ contrib/modular-transport/model/QUIC-sender/event-processors/QUIC-SendIfPossible.h
@@ -1,5 +1,5 @@
-#ifndef QUIC_FLOW_CONTROLLER_H
-#define QUIC_FLOW_CONTROLLER_H
+#ifndef QUIC_SEND_IF_POSSIBLE_H
+#define QUIC_SEND_IF_POSSIBLE_H
 #include "QUIC-Dispatcher.h"
 #include "QUIC-EventProcessor.h"
 
@@ -12,19 +12,12 @@ class MTHeader;
 /**
  * \brief The processor handling the flow control events.
  */
-class QUICFlowController: public QUICEventProcessor
+class QUICSendIfPossible: public QUICEventProcessor
 {
-private:
-    int maxData;
-    int maxStreamData;
 public:
-    QUICFlowController();
+    QUICSendIfPossible();
     EventProcessorOutput* Process(MTEvent e, MTContext* c);
     bool IsValidEvent(MTEvent e);
-    void SetMaxStreamData(int value);
-    void SetMaxData(int value);
-    int GetMaxStreamData();
-    int GetMaxData();
 };
 } // namespace ns3
 #endif
