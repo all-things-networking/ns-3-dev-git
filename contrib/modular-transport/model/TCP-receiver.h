ffb6a5758495ffdc9adeb2841fe15b0f158b78e9
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-03-06 14:35:43 -0500

    all fix imports squshed
    forward declearation
    
    colons
    
    Update mt-receiver.h
    
    forward declear of enum
    
    normal pointers
    
    Update mt-receiver.h
    
    revert
    
    add import
    
    Update mt-receiver.h
    
    Update mt-receiver.h
    
    Update mt-receiver.h

--- contrib/modular-transport/model/TCP-receiver.h
+++ contrib/modular-transport/model/TCP-receiver.h
@@ -1,11 +1,8 @@
 #ifndef TCP_RECEIVER_H
 #define TCP_RECEIVER_H
 #include "mt-receiver.h"
+#include "ns3/ipv4-l3-protocol.h"
 #include "ns3/node.h"
-#include "ns3/ip-l4-protocol.h"
-#include "ns3/ipv4-address.h"
-#include "ns3/ipv6-address.h"
-#include "ns3/sequence-number.h"
 namespace ns3{
 class MTReceiver;
 class ModularTransport;
--- contrib/modular-transport/model/mt-receiver.h
+++ contrib/modular-transport/model/mt-receiver.h
@@ -1,13 +1,20 @@
 #ifndef MT_RECEIVER_H
 #define MT_RECEIVER_H
-#include "ns3/node.h"
 #include "ns3/ip-l4-protocol.h"
-#include "ns3/ipv4-address.h"
-#include "ns3/ipv6-address.h"
-#include "ns3/sequence-number.h"
+#include "ns3/ipv4-interface.h"
+#include "ns3/ipv4-l3-protocol.h"
+#include "ns3/ipv4-l3-protocol.h"
+#include "ns3/node.h"
+
 namespace ns3{
 class ModularTransport;
+class Packet;
+class IpL4Protocol;
+class Ipv4Header;
+class Ipv4Interface;
+
 class MTReceiver{
+
     public:
     MTReceiver();
     virtual enum IpL4Protocol::RxStatus Receive(
