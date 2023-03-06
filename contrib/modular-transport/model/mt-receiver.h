dbbdff600a0ee8757d3d6f2933d36f0f8854f77f
DannyDaoBoYang <34634047+DannyDaoBoYang@users.noreply.github.com>
2023-03-06 15:12:26 -0500

    Update mt-receiver.h

--- contrib/modular-transport/model/mt-receiver.h
+++ contrib/modular-transport/model/mt-receiver.h
@@ -2,9 +2,6 @@
 #define MT_RECEIVER_H
 #include "ns3/ip-l4-protocol.h"
 #include "ns3/ipv4-interface.h"
-#include "ns3/ipv4-l3-protocol.h"
-#include "ns3/ipv4-l3-protocol.h"
-#include "ns3/node.h"
 
 namespace ns3{
 class ModularTransport;
