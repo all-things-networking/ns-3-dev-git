b07245ec4205b4857656fb2637835a88dd635a9e
Sterling SHI <sterlingt3485@gmail.com>
2024-02-11 19:43:55 -0500

    finished event and struct

--- contrib/QUIC_TEL/model/QUIC-Event.cc
+++ contrib/QUIC_TEL/model/QUIC-Event.cc
@@ -10,149 +10,145 @@ namespace ns3{
 SendEvent::SendEvent(){
 
 }
-SendEvent::SendEvent(long time, int flow_id){
-    this->time=time;
+SendEvent::SendEvent(int flow_id){
     this->flow_id=flow_id;
 }
+
 AckEvent::AckEvent(){
-
-}
-AckEvent::AckEvent(int flow_id, int seq){
-    this->seq=seq;
-    this->flow_id=flow_id;
 }
 
-StreamEvent::StreamEvent(int flow_id, StreamEventType streamEventType, StreamEventData data, int stream_id){
-    this->streamEventType=streamEventType;
-    this->stream_id=stream_id;
+AckEvent::AckEvent(int flow_id) {
     this->flow_id=flow_id;
-    this->type = EventType::STREAM_EVENT;
+    this->largest = 0; //NOT SURE if these are initialized correctly
+    this->ECT0_Count = 0;
+    this->ECT1_Count = 0;
+    this->ECN_CE_Count = 0;
+    this->pkt_nums = std::vector<int>();
+}
+
+StreamEvent::StreamEvent(std::string data, int id = NO_STREAM_ID, int frame_size_limit) {
     this->data = data;
+    this->id=id;
+    this->frame_size_limit = frame_size_limit;
 }
 
-ResponseEvent::ResponseEvent(int flow_id, ResponseEventType responseEventType, ResponseEventData data, int stream_id){
-    this->responseEventType=responseEventType;
-    this->stream_id=stream_id;
-    this->flow_id=flow_id;
-    this->type = EventType::RESPONSE_EVENT;
-    this->data = data;
+TimerEvent::TimerEvent() {
+
 }
 
-ReceivePacketEvent::ReceivePacketEvent(long time, int flow_id, Packet* rcv){
-    this->flow_id = flow_id;
-    this->time = time;
-    this->type = EventType::RECEIVEPKT_EVENT;
-    this->receivered = rcv;
+CongestionEvent::CongestionPacketEvent(int time_sent) {
+    this->time_sent = time_sent;
 }
+    /* THESE are left until testing
+    MTEvent* SenderEventCreator::CreateSendEvent(int flow_id, long time){
+        // Create random data for now
+        StreamEventData data;
+        data.text = "hello";
 
-MTEvent* SenderEventCreator::CreateSendEvent(int flow_id, long time){
-    // Create random data for now
-    StreamEventData data;
-    data.text = "hello";
-
-    // TODO: need to free this memory after?
-    MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::ADD_DATA, data, 5); // pick random stream_id for now
-    return streamEvent;
-}
-
-MTEvent* SenderEventCreator::CreateSendPacketEvent(int flow_id, long time){
-    StreamEventData data;
-    data.text = "";
-
-    MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::SEND_PACKET, data, -1);
-    return streamEvent;
-}
-
-MTEvent* SenderEventCreator::CreateAddDataEvent(int flow_id, long time, std::string text, int stream){
-    StreamEventData data;
-    data.text = text;
-
-    MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::ADD_DATA, data, stream);
-    return streamEvent;
-}
-
-MTEvent* SenderEventCreator::CreateACKPacketEvent(int flow_id, long time, int packetNum){
-    ResponseEventData data;
-    data.text = "";
-    data.packetNum = packetNum;
-
-    MTEvent* streamEvent = new ResponseEvent(flow_id, ResponseEventType::ACK_PACKET, data, -1);
-    return streamEvent;
-}
-
-MTEvent* ReceiverEventCreator::CreateReceiveEvent(int flow_id, long time, Packet* pkg){
-    MTEvent* RCVPacketEvent = new ReceivePacketEvent(time, flow_id, pkg); 
-    return RCVPacketEvent;
-}
-
-
-Packet* ReceiverEventCreator::CreateFakePacket(std::vector<std::string>& data, int packetNumber, bool setFinBit, int streamOffset, int streamContentOffset ){
-    // TODO: make this more flexible
-
-    QUICPacketBuffer* PacketBuffer = new QUICPacketBuffer;
-
-    int MAX_STREAM_DATA = 5; // hard coded
-    std::vector<int> offset( data.size(), 0 + streamContentOffset );
-
-    Ptr<Packet> QUICPacket = Create<Packet>();
-
-    // create short header
-    // QUICPacket should have a short header
-    // long header -> used for packets sent prior to establishment of 1-RTT keys
-    MTQUICShortHeader quicHeader = MTQUICShortHeader();
-    quicHeader.pckNum = packetNumber;
-    QUICPacket->AddHeader( quicHeader );
-
-    int totalLength = 0;
-    for ( auto& streamData : data ){
-        totalLength += streamData.size();
+        // TODO: need to free this memory after?
+        MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::ADD_DATA, data, 5); // pick
+    random stream_id for now return streamEvent;
     }
-    
-    int processedLength = 0;
-    int i = 0;
-    while ( true ){
-        if ( processedLength == totalLength ) break;
 
-        int currentIdx = i % data.size();
-        std::string& currentData = data[ currentIdx ];
-        
-        if ( currentData.size() == 0 ){
-            i++;
-            continue;
+    MTEvent* SenderEventCreator::CreateSendPacketEvent(int flow_id, long time){
+        StreamEventData data;
+        data.text = "";
+
+        MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::SEND_PACKET, data, -1);
+        return streamEvent;
+    }
+
+    MTEvent* SenderEventCreator::CreateAddDataEvent(int flow_id, long time, std::string text, int
+    stream){ StreamEventData data; data.text = text;
+
+        MTEvent* streamEvent = new StreamEvent(flow_id, StreamEventType::ADD_DATA, data, stream);
+        return streamEvent;
+    }
+
+    MTEvent* SenderEventCreator::CreateACKPacketEvent(int flow_id, long time, int packetNum){
+        ResponseEventData data;
+        data.text = "";
+        data.packetNum = packetNum;
+
+        MTEvent* streamEvent = new ResponseEvent(flow_id, ResponseEventType::ACK_PACKET, data, -1);
+        return streamEvent;
+    }
+
+    MTEvent* ReceiverEventCreator::CreateReceiveEvent(int flow_id, long time, Packet* pkg){
+        MTEvent* RCVPacketEvent = new ReceivePacketEvent(time, flow_id, pkg);
+        return RCVPacketEvent;
+    }
+
+
+    Packet* ReceiverEventCreator::CreateFakePacket(std::vector<std::string>& data, int packetNumber,
+    bool setFinBit, int streamOffset, int streamContentOffset ){
+        // TODO: make this more flexible
+
+        QUICPacketBuffer* PacketBuffer = new QUICPacketBuffer;
+
+        int MAX_STREAM_DATA = 5; // hard coded
+        std::vector<int> offset( data.size(), 0 + streamContentOffset );
+
+        Ptr<Packet> QUICPacket = Create<Packet>();
+
+        // create short header
+        // QUICPacket should have a short header
+        // long header -> used for packets sent prior to establishment of 1-RTT keys
+        MTQUICShortHeader quicHeader = MTQUICShortHeader();
+        quicHeader.pckNum = packetNumber;
+        QUICPacket->AddHeader( quicHeader );
+
+        int totalLength = 0;
+        for ( auto& streamData : data ){
+            totalLength += streamData.size();
         }
-        
-        std::string currentFrameData = currentData.substr( 0, MAX_STREAM_DATA );
-        
-        // process input
-        currentData = currentData.substr( currentFrameData.length() );
-        processedLength += currentFrameData.size();
 
-        int currentStreamNumber = currentIdx + 1 + streamOffset;
-        QUICFrame* currentFrame = new QUICFrame;
-        int finBit = currentData.size() == 0 && setFinBit ? 1 : 0;
+        int processedLength = 0;
+        int i = 0;
+        while ( true ){
+            if ( processedLength == totalLength ) break;
 
-        // std::cout << offset[ currentIdx ] << std::endl;
-        std::cout << currentFrameData << " " << currentStreamNumber << " " << offset[ currentIdx ] << std::endl;
-        QUICFrameHeader currentFrameHeader = QUICFrameHeader(currentStreamNumber, offset[ currentIdx ], currentFrameData.size(), FrameType::STREAM, finBit,
-            0, 0, 0);
-        
-        offset[ currentIdx ] += currentFrameData.size();
+            int currentIdx = i % data.size();
+            std::string& currentData = data[ currentIdx ];
 
-        Ptr<Packet> currentFrameDataPacket = Create<Packet>(reinterpret_cast<const uint8_t*>(currentFrameData.data()), currentFrameData.size());
-        currentFrame->AddHeader( currentFrameHeader );
-        currentFrame->AddAtEnd( currentFrameDataPacket );
-        QUICPacket->AddAtEnd( currentFrame );
-        i++;
+            if ( currentData.size() == 0 ){
+                i++;
+                continue;
+            }
+
+            std::string currentFrameData = currentData.substr( 0, MAX_STREAM_DATA );
+
+            // process input
+            currentData = currentData.substr( currentFrameData.length() );
+            processedLength += currentFrameData.size();
+
+            int currentStreamNumber = currentIdx + 1 + streamOffset;
+            QUICFrame* currentFrame = new QUICFrame;
+            int finBit = currentData.size() == 0 && setFinBit ? 1 : 0;
+
+            // std::cout << offset[ currentIdx ] << std::endl;
+            std::cout << currentFrameData << " " << currentStreamNumber << " " << offset[ currentIdx
+    ]
+    << std::endl; QUICFrameHeader currentFrameHeader = QUICFrameHeader(currentStreamNumber, offset[
+    currentIdx ], currentFrameData.size(), FrameType::STREAM, finBit, 0, 0, 0);
+
+            offset[ currentIdx ] += currentFrameData.size();
+
+            Ptr<Packet> currentFrameDataPacket = Create<Packet>(reinterpret_cast<const
+    uint8_t*>(currentFrameData.data()), currentFrameData.size()); currentFrame->AddHeader(
+    currentFrameHeader ); currentFrame->AddAtEnd( currentFrameDataPacket ); QUICPacket->AddAtEnd(
+    currentFrame ); i++;
+        }
+
+        std::cout << "Creating Fake Receiving Packet, packet number: " << quicHeader.pckNum <<
+    std::endl;
+        // Ptr<Packet> ptrPacket = PacketBuffer->CreatePacket();
+        Packet* pkt = GetPointer( QUICPacket );
+        return pkt;
     }
+    */
 
-    std::cout << "Creating Fake Receiving Packet, packet number: " << quicHeader.pckNum << std::endl;
-    // Ptr<Packet> ptrPacket = PacketBuffer->CreatePacket();
-    Packet* pkt = GetPointer( QUICPacket );
-    return pkt;
-}
-
-QUICEvent::QUICEvent(){
-
+QUICEvent::QUICEvent() {
 }
 
 QUICEvent::QUICEvent(long time, int flow_id, EventType type){
--- contrib/QUIC_TEL/model/QUIC-Event.h
+++ contrib/QUIC_TEL/model/QUIC-Event.h
@@ -33,7 +33,7 @@ class Frame {
     int data_length;
     int stream_id;
     int offset;
-    std::string data; // Try to correspond this field with stream data in the TEL DSL
+    std::string data; // Try to correspond this field with "stream data" in the TEL DSL
 };
 
 // Reponse event type from the receiver
@@ -42,47 +42,51 @@ class MTHeader;
 class MTEvent;
 class SendEvent: public QUICEvent{
     public:
+    int flow_id;
     SendEvent();
     SendEvent(int flow_id);
 };
 
 class AckEvent: public QUICEvent{
     public:
-    int seq;
-    AckEvent();
-    /*
-    list<int> pkt_nums;
+    int flow_id;
+    std::vector<int> pkt_nums; //list<int> pkt_nums;
     int largest;
     int ECT0_Count;
     int ECT1_Count;
     int ECN_CE_Count;
-    */
-    AckEvent(int flow_id, int seq);
+    AckEvent();
+    AckEvent(int flow_id);
 };
 
-class StreamEvent: public QUICEvent{
+class AddDataEvent : public QUICEvent {
     public:
-    StreamEventType streamEventType; // This is used to identify the type of stream event
-    int stream_id;
+    bool server_side;
+    bool direction;
+    std::string data;//stream data;
+    AddDataEvent();
+    AddDataEvent();
+};
+
+class StreamEvent : public QUICEvent { // trying to match this up with the struct QuicStream in the TEL DSL, not sure if I am doing
+    // NOT sure is this is written correctly
+    public:
+    std::string data; // stream data;
+    int id;
+    int frame_size_limit;
+    int last_sent;
+    std::vector<bool> acked_frames; // list<bool> acked_frames;
     StreamEvent();
 
-    StreamEventData data;
-    StreamEvent(int flow_id, StreamEventType streamEventType, StreamEventData data, int stream_id = NO_STREAM_ID);
+    StreamEvent(std::string data, int id = NO_STREAM_ID, int frame_size_limit);
 };
 
-class ResponseEvent: public QUICEvent{
+// This one would be modified after finishing all the implementation and before testing
+class SenderEventCreator
+{
     public:
-    ResponseEventType responseEventType; // This is used to identify the type of response event
-    int stream_id; // Might not be needed in response event
-    ResponseEventData data;
-
-    ResponseEvent();
-    ResponseEvent(int flow_id, ResponseEventType responseEventType, ResponseEventData data, int stream_id = NO_STREAM_ID);
-};
-
-class SenderEventCreator {
-    public:
-    // These events are used mainly for testing right now - it creates the specific event based on input
+    // These events are used mainly for testing right now - it creates the specific event based
+    // on input
     MTEvent* CreateSendEvent(int, long);
     MTEvent* CreateSendPacketEvent(int flow_id, long time);
     MTEvent* CreateAddDataEvent(int flow_id, long time, std::string text, int stream);
@@ -92,40 +96,49 @@ class SenderEventCreator {
 
 //////////////////////////// Receiver //////////////////////////
 /**
- * Event for application layer updates
-*/
-class AppUpdateEvent: public QUICEvent{
+ * NOT SURE IF IT is useful for our translation: Event for application layer updates
+ */
+class AppUpdateEvent : public QUICEvent
+{
     public:
     AppUpdateEvent();
     AppUpdateEvent(long time, int flow_id);
 };
 
 /**
- * Event for tracking ACK time
-*/
-class ACKTimerEvent: public QUICEvent{
+ * TIMER in TEL DSL is currently empty
+ */
+class TimerEvent : public QUICEvent
+{
     public:
-    ACKTimerEvent();
-    ACKTimerEvent(long time, int flow_id);
+    TimerEvent();
+    //TimerEvent(long time, int flow_id);
 };
 
 /**
- * Event for receiving a packet
-*/
-class ReceivePacketEvent: public QUICEvent{
+ * Congestion in TEL DSL
+ */
+class CongestionEvent : public QUICEvent
+{
     public:
-    Packet* receivered;
-    ReceivePacketEvent();
-    ReceivePacketEvent(long time, int flow_id, Packet* rcv);
+    int time_sent;
+    CongestionPacketEvent();
+    CongestionPacketEvent(int time_sent);
 };
 
-class ReceiverEventCreator {
+// This one would be modified after finishing all the implementation and before testing
+class ReceiverEventCreator
+{
     public:
-    // These events are used mainly for testing right now - it creates the specific event based on input
+    // These events are used mainly for testing right now - it creates the specific event based
+    // on input
     MTEvent* CreateReceiveEvent(int, long, Packet* packet);
-    Packet* CreateFakePacket(std::vector<std::string>& data, int packetNumber, bool setFinBit, int streamOffset, int streamContentOffset = 0);
+    Packet* CreateFakePacket(std::vector<std::string>& data,
+                                int packetNumber,
+                                bool setFinBit,
+                                int streamOffset,
+                                int streamContentOffset = 0);
 };
-
 
 /////////////////////////////////////////////////////////////
 }
