1b832dd5175eb2968d59105e882d78c91c4545a8
Sterling SHI <sterlingt3485@gmail.com>
2024-02-27 15:45:37 -0500

    fixing part of selectDataProcessor

--- contrib/QUIC_TEL/helper/QUIC-selectDataProcessor.cc
+++ contrib/QUIC_TEL/helper/QUIC-selectDataProcessor.cc
@@ -2,13 +2,14 @@
 
 #include "../model/QUIC-Context.h"
 #include "../model/QUIC-Event.h"
+#include "QUIC_TEL-QuicHeader.h"
 #include "QUIC-Stream.h"
-
-#include "QUIC-Frame.h"
+#include "QUIC_TEL-Frame.h"
 
 #include "ns3/mt-eventprocessor.h"
 
 #include <algorithm> // added for min function
+#include <chrono>
 #include <ctime>
 #include <map>
 
@@ -31,7 +32,7 @@ namespace ns3
     {
         int sent_size = 0;
         int i = 0;
-        Packet pkt = Packet(); // packet
+        Packet pkt;
         QUICStream qs = QUICStream(0);         // TODO: better definition of QUICStream
         int bytes_allowed = ctx.congestion_window - ctx.bytes_in_flight;
         int frame_size_limit = bytes_allowed / ctx.streams.size(); // len changed to size here
@@ -39,21 +40,21 @@ namespace ns3
         {
             PacketInfo pkt_info;
             while (sent_size < ctx.size_limit && i < ctx.streams.size() && bytes_allowed > 0)   {
-                qs = ctx.streams[i];
-                Frame frame; // TODO: Frame need redefinition
+                qs = *ctx.streams[i];
+                Frame frame = Frame(0,0,0); // TODO: Frame need redefinition
                 pkt_info.stream_id.emplace_back(i); // add changed to emplace_back
                 auto min_value = std::min({qs.frame_size_limit,
                                         bytes_allowed,
                                         frame_size_limit,
                                         static_cast<int>(qs.data.size()) - qs.last_sent}); // TODO: change the usage of this MIN
-                pkt_info.frame_size.emplace_back(frame.size); // add changed to emplace_back
-                pkt_info.start_point.emplace(qs.last_sent);   // add changed to emplace_back
+                pkt_info.frame_size.emplace_back(frame.size); // add changed to emplace_back, is Frame.size just the data_length?
+                pkt_info.start_point.emplace_back(qs.last_sent);   // add changed to emplace_back
 
                 frame.data = std::vector<char>(qs.data.begin() + qs.last_sent, 
                                                 qs.data.begin() + qs.last_sent + frame.size); // frame.data = qs.data [qs.last_sent:qs.last_sent + frame.size];
 
                 qs.last_sent = qs.last_sent + frame.size;
-                pkt.add_data(std::byte(frame));// add std:: before byte
+                Packet pkt = Packet(reinterpret_cast<uint8_t*> (&frame), sizeof(reinterpret_cast<uint8_t*> (&frame))); // add std:: before byte
                 ctx.bytes_in_flight = ctx.bytes_in_flight + frame.data.size(); // len to size
                 bytes_allowed = bytes_allowed - frame.data.size(); // len to size
                 i = i + 1;
